b5ef834550023d292ad8dedeb5dc2c08e6326719
cedar <186581553+Cedar-Crow@users.noreply.github.com>
2025-11-20 12:01:06 +0100

    Copy exercise starting code

---
+++ week-1/les-2/dropdown_exercise/main.js
@@ -0,0 +1,27 @@
+const dropdownMenu = {
+    label: "Dropdown",
+    items: [
+        "Item 1",
+        "Item 2",
+        {
+            label: "Submenu 1",
+            items: [
+                "Subitem 1.1",
+                "Subitem 1.2",
+                {
+                    label: "Sub-submenu 1",
+                    items: ["Sub-subitem 1.1", "Sub-subitem 1.2", "Sub-subitem 1.3"],
+                },
+            ],
+        },
+        "Item 3",
+        {
+            label: "Submenu 2",
+            items: ["Subitem 2.1", "Subitem 2.2"],
+        },
+    ],
+};
+
+const dropdownContainer = document.getElementById("dropdown-container");
+dropdownContainer.appendChild(generateDropdownHTML(dropdownMenu));
+
