8969d8347571f02f927c2d387e1a299ef863e1fa
cedar <186581553+Cedar-Crow@users.noreply.github.com>
2025-12-01 15:59:24 +0100

    Make exercise 3

--- week-2/les-2/array_exercises.js
+++ week-2/les-2/array_exercises.js
@@ -4,3 +4,14 @@
 
 //2
 // -> will alert 4 ('Bananas was added, so there are 4 items now)
+
+//3
+const matrix = [[74, 85, 96], [41, 52, 63]];
+
+function logMatrixNumber(column, row) {
+    const selectedNumber = matrix[row][column];
+    console.log(selectedNumber);
+}
+
+logMatrixNumber(2, 0);
+
