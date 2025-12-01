c02488a31f3479eaea6413955e35676639675e1a
cedar <186581553+Cedar-Crow@users.noreply.github.com>
2025-12-01 15:47:35 +0100

    Answer question 2

--- week-2/les-2/array_exercises.js
+++ week-2/les-2/array_exercises.js
@@ -1,3 +1,6 @@
 // 1
-const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-weekdays.forEach((currDay) => console.log(currDay));
+// const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+// weekdays.forEach((currDay) => console.log(currDay));
+
+//2
+// -> will alert 4 ('Bananas was added, so there are 4 items now)
