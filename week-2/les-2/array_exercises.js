fdbc960053c9c6d4b906d15318813a53a76c4d67
cedar <186581553+Cedar-Crow@users.noreply.github.com>
2026-01-07 21:25:10 +0100

    Make exercise 4

--- week-2/les-2/array_exercises.js
+++ week-2/les-2/array_exercises.js
@@ -6,12 +6,29 @@
 // -> will alert 4 ('Bananas was added, so there are 4 items now)
 
 //3
-const matrix = [[74, 85, 96], [41, 52, 63]];
+// const matrix = [[74, 85, 96], [41, 52, 63]];
 
-function logMatrixNumber(column, row) {
-    const selectedNumber = matrix[row][column];
-    console.log(selectedNumber);
+// function logMatrixNumber(column, row) {
+//     const selectedNumber = matrix[row][column];
+//     console.log(selectedNumber);
+// }
+
+// logMatrixNumber(2, 0);
+
+//4
+const numbers = [1, 2, 3, 4, 5];
+const string = "Hello";
+const colors = ['red', 'blue', 'green', 'yellow'];
+
+for (const number of numbers) {
+    console.log(number);
+}
+for (const letter of string) {
+    console.log(letter);
 }
 
-logMatrixNumber(2, 0);
+colors.forEach((color, index) => {
+    console.log(color, index);
+
+});
 
